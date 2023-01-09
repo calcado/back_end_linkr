5a45d69c5ad537fcae09ab97119b3fc5c1448fb2
Igor de Castro <igordecastro07@gmail.com>
2023-01-08 23:13:01 -0300

    git merge

--- src/routes/timeline_post.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import {timeline_post} from "../controllers/timeline_post.controller.js";
-
-  const router = Router();
-
-  router.post("/timeline_post", timeline_post);
-
-  export default router;
