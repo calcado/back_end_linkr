40e509a9e391f410b25d9e4b39479cf3ba5e8270
pedro_calcado_moreira <pecalcado@gmail.com>
2023-01-09 17:09:51 -0300

    userRoute

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import timeline_post from "./routes/timeline_post.route.js";
 import timeline_get from "./routes/timeline_get.route.js";
 import likeRoute from "./routes/likeRoutes.js"
 import dotenv from "dotenv"
-
+import userRoute from "./routes/usersRoutes.js";
 import timelineRoute from "./routes/timelineRoutes";
 
 dotenv.config();
@@ -29,7 +29,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(authRoute)
-
+app.use(userRoute)
 app.use(likeRoute)
 app.use(timelineRoute);
 
