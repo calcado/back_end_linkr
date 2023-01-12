1d923b8cbe574e9ca3f0420945230778df06e10b
Igor de Castro <igordecastro07@gmail.com>
2023-01-12 15:00:20 -0300

    fix: post trending

--- src/middlewares/trendingMiddleware.js
+++ src/middlewares/trendingMiddleware.js
@@ -1,7 +1,15 @@
-import { connection } from "../app.js";
+import trendRepository from "../repositories/trendingRepository.js";
 
 export async function trendingValidation(req, res, next) {
-    
+    const { hashtag } = req.params;
 
+    const { id } = await trendRepository.findHashtag(hashtag)
+        
+        if (!id) {
+            return res.status(404).send({message: "Hashtag não encontrada!"});
+        }
+
+        req.hashtagId = id;
+        
     next()
 }
--- src/routes/trendingRoutes.js
+++ src/routes/trendingRoutes.js
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import { connection } from "../app.js";
 import { trending, trendingPosts } from "../controllers/trendingController.js";
+import { trendingValidation } from "../middlewares/trendingMiddleware.js";
 
 
 const trendingRoute = Router()
 
 
 trendingRoute.get("/trending", trending)
-trendingRoute.get("/hashtag/:hashtag", trendingPosts)
+trendingRoute.get("/hashtag/:hashtag", trendingValidation, trendingPosts)
 //TESTE PARA PEGAR AS HASHTAGS
 trendingRoute.get("/hashtags", async (req,res) => {
-     const hashtags = await connection.query("SELECT * FROM trending;")
+     const hashtags = await connection.query('SELECT * FROM trending;')
      res.status(200).send(hashtags.rows)
 })
 
